cef3d94784a36c634adea6c951137c650653606f
harik31197 <harikrishnan.a@psiog.com>
2019-11-03 22:26:38 +0530

    Create mock-employee.ts

---
+++ src/app/appdata/mock-employee.ts
@@ -0,0 +1,14 @@
+import {Employee} from '../appdata/employee'
+
+export const EMPLOYEES: Employee[] = [
+    { id: 11, name: 'Kohli' },
+    { id: 12, name: 'Dhoni' },
+    { id: 13, name: 'Bumrah' },
+    { id: 14, name: 'Ganguly' },
+    { id: 15, name: 'Mongia' },
+    { id: 16, name: 'Rohit' },
+    { id: 17, name: 'Rahane' },
+    { id: 18, name: 'Sachin' },
+    { id: 19, name: 'Ashwin' },
+    { id: 20, name: 'Jadeja' }
+];
